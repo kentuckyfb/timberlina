e16c2cb6ef4dc56a614783de6648f23f339cd53e
kenuckyfb <nathanhimesh5@gmail.com>
2025-05-07 08:27:57 +0530

    Updated Products list

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -60,29 +60,8 @@ const Footer: React.FC = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                 </svg>
                 <a href="mailto:kushiiam23@gmail.com" className="hover:text-accent-teal transition-colors">
-                  kushiiam23@gmail.com
+                timberlina@timberconcepts.lk
                 </a>
-              </p>
-              <p className="mb-3 flex items-center">
-                <svg className="w-5 h-5 mr-2 text-accent-teal" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
-                </svg>
-                <a href="tel:+94776600877" className="hover:text-accent-teal transition-colors">
-                  +94 77 660 0877
-                </a>
-              </p>
-              <p className="mb-3 flex items-center">
-                <svg className="w-5 h-5 mr-2 text-accent-teal" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-                </svg>
-                <span>Kushali Amaratunga</span>
-              </p>
-              <p className="flex items-center">
-                <svg className="w-5 h-5 mr-2 text-accent-teal" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                </svg>
-                Mirigama, Sri Lanka
               </p>
             </address>
           </div>
